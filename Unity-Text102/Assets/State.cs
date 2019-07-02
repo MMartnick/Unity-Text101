be3b42173cb4efaba4a27297dd0005ecf0c827ab
Martnick1 <matthewmartnick@gmail.com>
2019-07-02 15:06:12 -0400

    text adventure update

---
+++ Unity-Text102/Assets/State.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+  // Text area numerals set the min and max hight of the unity text box before adding a scroll bar
+  [TextArea(10,14)] [SerializeField] string storyText;
+  
+}
