b14f5beaaed3fe8c8e85f66ef9e454ebb93d2477
Martnick1 <matthewmartnick@gmail.com>
2019-07-03 16:52:31 -0400

    text adventure update / States added

--- text102/Assets/AdventureGame.cs
+++ text102/Assets/AdventureGame.cs
@@ -8,6 +8,10 @@ public class AdventureGame : MonoBehaviour
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
+    // int and string array example
+    int[] oddNumbers = {1, 3 , 5 , 7 , 9};
+    string[] daysOfWeek = {"mon", "tues", "wens", "thurs"};
+
     State state;
 
     // Start is called before the first frame update
@@ -15,6 +19,8 @@ public class AdventureGame : MonoBehaviour
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
+        Debug.Log(oddNumbers[3]);
+        Debug.Log(daysOfWeek[1]);
     }
 
     // Update is called once per frame
